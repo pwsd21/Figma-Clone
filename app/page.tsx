f575dc532aa34d43ca5ffc94f8a324562cbeeb4b
pwsd21 <pawansachdeva1998@gmail.com>
2024-02-08 15:58:33 +0530

    Liveblocks and fabric.js setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
     <Room>
-      <CollaborativeApp />
+      <h1 className="text-4xl text-white">LiveBlocks Figma Clone</h1>
     </Room>
   );
 }
